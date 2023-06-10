19c33c5382a2c53fdbca99b341b187bcbbd46fbc
Jonath-z <jonathanzihindula95@gmail.com>
2023-06-10 12:11:00 +0200

    feat: setup docker and add PR template

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = "https://news-proxy.netlify.app/api"
+export const API_BASE_URL =
+  process.env.REACT_APP_PROXY_URL ||
+  "https://news-proxy.netlify.app/api"
 export const LATEST_NEWS_MAXIMUM_RESULT_SIZE = 10
 export const SEARCH_RESULT_MAXIMUM_SIZE = 10
 export const SEARCH_RESULT_BY_PUBLISHER_MAXIMUM_SIZE = 50
